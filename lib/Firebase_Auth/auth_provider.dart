16dc337d24ccde2053a1fd7d916994a37c5f76b4
Hamoud47 <muhammedtalal2004@gmail.com>
2024-08-04 15:06:32 +0800

    message

---
+++ lib/Firebase_Auth/auth_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthProvider with ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? _user;
+
+  AuthProvider() {
+    _auth.authStateChanges().listen((User? user) {
+      _user = user;
+      notifyListeners();
+    });
+  }
+
+  User? get user => _user;
+  String? get email => _user?.email;
+
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      print('Sign-in error: $e');
+    }
+  }
+}
