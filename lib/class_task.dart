4a64990c4b4aecdf65ce8af16fe7da09abd6fc5a
Shayaan Dhuny <shayaandhuny0302@gmail.com>
2024-08-05 20:49:10 +0800

    New Layout

---
+++ lib/class_task.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:ggt_assignment/model/maintenance_task.dart';
+
+class Task with ChangeNotifier {
+  List<MaintenanceTask> _tasks = [];
+
+  List<MaintenanceTask> get tasks => _tasks;
+
+  void addTask(MaintenanceTask task) {
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(MaintenanceTask updatedTask) {
+    int index = _tasks.indexWhere((task) => task.taskID == updatedTask.taskID);
+    if (index != -1) {
+      _tasks[index] = updatedTask;
+      notifyListeners();
+    }
+  }
+
+  void deleteTask(String taskID) {
+    _tasks.removeWhere((task) => task.taskID == taskID);
+    notifyListeners();
+  }
+}
